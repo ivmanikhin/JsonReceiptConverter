f8c8841190787549ffb50aa0bf2fe0fb635818db
ivmanikhin <ivmanikhin@gmail.com>
2022-04-05 09:56:26 +0300

    now i can send json to my app from anywhere except FNS app. started to work with JSON

--- app/src/main/java/com/manikhin/jsonreceiptconverter/MainActivity.java
+++ app/src/main/java/com/manikhin/jsonreceiptconverter/MainActivity.java
@@ -1,6 +1,5 @@
 package com.manikhin.jsonreceiptconverter;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -10,19 +9,19 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.FileObserver;
+import android.net.Uri;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import android.os.Bundle;
 import android.view.View;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView_human_text;
@@ -51,7 +50,39 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
         }
     }
+    private void handleJsonData(Intent intent) {
+        Uri json_uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (json_uri != null) {
+            String data, buff = "";
+            try {
+                final String[] split = json_uri.getPath().split(":");
+                File file = new File(split[1]);
+//                File file = new File(json_uri.getPath());
+                FileInputStream fin = new FileInputStream(file);
+                BufferedReader br = new BufferedReader( new InputStreamReader(fin));
+                while((data = br.readLine()) != null){
+                    buff += data;
+                }
 
+                JSONObject receipt = new JSONObject(buff);
+
+                JSONArray items = receipt.getJSONArray("items");
+                String[] item_names = new String[items.length()];
+                for (int i=0; i<items.length(); i++) {
+                    item_names[i] = items.getString(i);
+                }
+
+
+                textView_human_text.setText(item_names[1]);
+                br.close();
+                fin.close();
+                Toast.makeText(getBaseContext(), "Data has been read", Toast.LENGTH_LONG).show();
+            }
+            catch (Exception e){
+                Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +97,19 @@ public class MainActivity extends AppCompatActivity {
         else {
             requestStoragePermission();
         }
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String action = intent.getAction();
+            String type = intent.getType();
+            if (Intent.ACTION_SEND.equals(action) && type != null) {
+                handleJsonData(intent);
+            }
+        }
+
     }
+
+
 
     public void read_JSON_file(View view) {
 
@@ -96,14 +139,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == STORAGE_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Permission NOT granted", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode == STORAGE_PERMISSION_CODE) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "Permission NOT granted", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 }
